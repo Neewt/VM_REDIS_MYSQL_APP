a77a8ab363849d83c98d3e9be86b50e50741e544
Neewt <alex.lemire59@live.fr>
2022-12-12 14:18:56 +0100

    Nouvel appli fonctionne

--- application/app.py
+++ application/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import redis
 import mysql.connector
 
@@ -20,12 +20,40 @@ def index():
     # Increment the number of visits in Redis
     visits = redis_db.incr("visits")
 
-    # Retrieve the latest message from MySQL
+    # Retrieve the latest messages from MySQL
     cursor = mysql_db.cursor()
-    cursor.execute("SELECT message FROM messages ORDER BY id DESC LIMIT 1")
-    message = cursor.fetchone()
+    cursor.execute("SELECT message FROM messages ORDER BY id DESC LIMIT 10")
+    messages = cursor.fetchall()
 
-    return f"You are visitor number {visits}. Latest message: {message}"
+    return f"""
+        <html>
+            <body>
+                <h1>You are visitor number {visits}.</h1>
+                <p>Latest messages:</p>
+                <ul>
+                    {
+                        "".join([f"<li>{message}</li>" for message in messages])
+                    }
+                </ul>
+                <form method="post">
+                    <input type="text" name="message" />
+                    <input type="submit" value="Send" />
+                </form>
+            </body>
+        </html>
+    """
+
+@app.route("/", methods=["POST"])
+def add_message():
+    # Retrieve the message from the form
+    message = request.form["message"]
+
+    # Store the message in the database
+    cursor = mysql_db.cursor()
+    cursor.execute("INSERT INTO messages (message) VALUES (%s)", (message,))
+    mysql_db.commit()
+
+    return "Message sent!"
 
 if __name__ == "__main__":
     app.run()
